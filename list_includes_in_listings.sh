b36af57b50dabf541c5d21333ab90e90a5bc1a34
ATTY Lionel <yoyonel@hotmail.com>
2016-03-25 11:21:33 +0100

    rajout d'un répertoire pour les scripts. Mise à jour du CMakeLists (mais rien de transcendant)

--- list_includes_in_listings.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find src -type f -exec grep -H '#include' {} \;
--- list_pair_numbers_listings_with_main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# url: http://www.linfo.org/tr.html
-find src -type f -exec grep -H 'main(' {} \; | egrep -o '+[0-9].+[0-9]' | tr ' \n' ';'
